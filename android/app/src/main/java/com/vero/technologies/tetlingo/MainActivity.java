cd16807ae950c61de26e9632935e88cfbf810f97
vero-technologies <med.novera@gmail.com>
2026-06-26 16:25:11 +0000

    Add Android support

---
+++ android/app/src/main/java/com/vero/technologies/tetlingo/MainActivity.java
@@ -0,0 +1,5 @@
+package com.vero.technologies.tetlingo;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.vero.technologies.tetlingo',
+  appName: 'TetLingo',
+  webDir: 'dist'
+};
+
+export default config;
